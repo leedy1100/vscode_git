936726d1991d20bb9dc097c3577bebd463a8de49
leedy1100 <dy921119@gmail.com>
2020-01-03 19:49:53 +0900

    date conf

--- javaScript/js/date.js
+++ javaScript/js/date.js
@@ -25,28 +25,56 @@ const setSearchDates = (startDate, endDate, type, value) => {
     const today = new Date();
     dayAdd(today, type, value);
     console.log('day:'+dayAdd(today, type, value));
-    let fromDate = $('#fromDate').val(dayAdd(today, type, value));
+    const fromDate = $('#fromDate').val(dayAdd(today, type, value));
     console.log('fromDate : ' + fromDate);
     // $(endDate).val(today);
     
     
 };
 
+/**
+ * minDate config
+ * @param {날짜 속성} type 
+ * @param {minValue} value 
+ */
+const minDateConf = (type,value)=>{
+
+    let minDate = new Date();
+    console.log(minDate);
+    let minDate2;
+
+    if (type === 'month') {
+        minDate2 = minDate.getMonth() + value;
+        minDate.setMonth(minDate2);
+    }else if(type==='date'){
+        minDate2 = minDate.getDate() + value;
+        minDate.setDate(minDate2);
+    }
+
+    return minDate;
+}
+
 const initSearchDates = (startDate, endDate, type, value) => {
+
+    let minDate = new Date();
+    let minDate2 = minDate.getMonth()-1;
+    minDate.setMonth(minDate2);
 
 	const options = { 
         useCurrent: true,
 		format: "YYYY-MM-DD HH:mm",
 		locale: moment.locale('ko'),
 		defaultDate : new Date(), // 기본값으로 오늘 날짜를 입력한다. 기본값을 해제하려면 defaultDate 옵션을 생략한다. 
-		sideBySide:true,
+        sideBySide:true,
+        maxDate:new Date(), //최대 범위
+        minDate:minDateConf('month',-1) //최소 범위
 	};
 	
 	$(startDate).datetimepicker(options);
 	$(endDate).datetimepicker(options);
 
-    let dateType = 'date';
-    let dateValue = -3;
+    let dateType;
+    let dateValue;
 
 	if(isNoValue(type)) {
 		dateType = 'date';
